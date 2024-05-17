3cda5ce2cb2b02c7c7dc08ae1157ba912cb00d41
dombonifacio <nikkibonifacio184@gmail.com>
2024-05-17 14:01:16 -0700

    Fixed Proxy Server URL

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -12,7 +12,7 @@ export default defineConfig({
       "/api": {
         target: "https://upmeet.onrender.com",
         changeOrigin: true,
-       
+        rewrite: (path) => path.replace(/^\/api/, ""),
       },
     },
   },
