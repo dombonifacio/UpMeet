052a421fa46584f41417ee4dcb0d1187973b5724
dombonifacio <nikkibonifacio184@gmail.com>
2024-05-22 20:58:10 -0700

    removed cors configurations.

--- server/src/server.ts
+++ server/src/server.ts
@@ -29,17 +29,7 @@ const MONGODB_URI =
 // allows us to parse cookies
 app.use(cookieParser());
 // allows which domains are able to make requests to a web server
-app.use(
-  cors({
-    // don't forget the colon between base url and base port
-    // listen for incoming requests in this url
-    // origin: `${BASE_URL}:${BASE_PORT}`,
-    origin: "https://up-meet.vercel.app/",
-    
-    // include credentials such as cookies
-    credentials: true,
-  })
-);
+app.use(cors())
 
 // automatically parse incoming JSON  data
 app.use(express.json());
