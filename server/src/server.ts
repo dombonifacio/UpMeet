85f2bf62ed6c991bafd05c32782844ef24baa76b
dombonifacio <nikkibonifacio184@gmail.com>
2024-05-22 21:03:23 -0700

    added options request.

--- server/src/server.ts
+++ server/src/server.ts
@@ -35,19 +35,14 @@ app.use(
     // listen for incoming requests in this url
     // origin: `${BASE_URL}:${BASE_PORT}`,
     origin: "https://up-meet.vercel.app",
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     
     // include credentials such as cookies
     credentials: true,
   })
 );
 
-app.options("/api/auth/login", (req, res) => {
-  res.header("Access-Control-Allow-Origin", "https://up-meet.vercel.app");
-  res.header("Access-Control-Allow-Methods", "POST");
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  res.header("Access-Control-Allow-Credentials", "true");
-  res.sendStatus(200);
-});
+
 
 // automatically parse incoming JSON  data
 app.use(express.json());
