d6f9c5051a1d74119a2a375944a7613cecb8ec78
dombonifacio <nikkibonifacio184@gmail.com>
2024-07-22 23:30:12 -0700

    add

--- server/controllers/userController.ts
+++ server/controllers/userController.ts
@@ -16,7 +16,7 @@ export const getUserInfo = async (
     if (!user) {
       return res.status(404).json({ message: "User not found" });
     }
-    return res.status(200).json(user);
+    return res.status(200).json({message: "User is this", data: user});
   } catch (err) {
     return next(err);
   }
