575174c8ab6fe90c0c6d01623ba9c3f18650ca09
dombonifacio <nikkibonifacio184@gmail.com>
2024-07-24 20:27:43 -0700

    Add.

--- client/src/pages/ProfilePage.tsx
+++ client/src/pages/ProfilePage.tsx
@@ -1,4 +1,4 @@
-import axios, { AxiosError, AxiosResponse } from "axios";
+import axios, {  AxiosResponse } from "axios";
 import { useContext, useEffect, useState } from "react";
 
 import camera from "../assets/camera.jpg";
